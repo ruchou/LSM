0f80cb72f4c66e84a0d1acbe376642927d5ad89f
yanrujhou <billchou@sinica.edu.tw>
2021-09-09 16:07:19 +0800

    add lemmas

--- src/Lemmas.cpp
+++ src/Lemmas.cpp
@@ -17,3 +17,57 @@ void mergeLeft_append(std::map<T, std::optional<T>> Vn,
 
 
 }
+
+template<class T>
+void mergeRight_append(std::map<T, std::optional<T>> Vn,
+                       std::set<T> Es,
+                       std::map<T, std::optional<T>> Vm
+) {
+
+
+}
+
+void set_of_empty_map() {
+
+}
+
+void set_of_map_append(std::map<T, std::optional<T>> Cn,
+                       std::map<T, std::optional<T>> Cm
+) {
+
+}
+
+template<class T>
+void dom_append_right(std::map<T, std::optional<T>> Cn,
+                      std::map<T, std::optional<T>> Cm
+) {
+
+}
+
+template<class T>
+void node_sep_star(Node *r,
+                   Node *n,
+                   std::map<Node *, std::set<T>> esn,
+                   std::map<Node *, std::set<T>> esn1,
+                   std::map<T, std::optional<T>> V,
+                   std::map<T, std::optional<T>> V1
+) {
+
+}
+
+void node_es_disjoint(Node *r,
+                      Node *n,
+                      std::map<Node *, std::set<T>> esn,
+                      std::map<T, std::optional<T>> V
+) {
+
+}
+
+template<class T>
+void node_es_empty(Node *r,
+                   Node *n,
+                   std::map<Node *, std::set<T>> esn,
+                   std::map<T, std::optional<T>> V
+) {
+
+}
